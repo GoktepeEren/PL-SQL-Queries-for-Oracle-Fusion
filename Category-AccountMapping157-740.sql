23b02ea76e99d8f0a8f7ee1faeb76b185b0f8e22
GoktepeEren <goktepeeren@yahoo.com>
2020-07-03 23:34:01 +0300

    query edited about ınner joins

--- Category-AccountMapping157-740.sql
+++ Category-AccountMapping157-740.sql
@@ -37,19 +37,22 @@ tbl_codeX.SEGMENT2 as EXP1,
 valAccoutN.Description as EXPNAME
 
 From EGP_CATEGORIES_VL tbl_cat 
-Left Join XLA_MAPPING_SETS_B tbl_mapb
-	Left Join XLA_MAPPING_SET_VALUES tbl_map
-       Inner Join GL_CODE_COMBINATIONS tbl_codeX
-			Inner Join FND_VS_VALUES_B valAccout
-				Inner Join FND_VS_VALUES_TL valAccoutN 
-				ON valAccout.Value_Id = valAccoutN.Value_Id 
-				and valAccoutN.LANGUAGE= FND_GLOBAL.Current_Language
-				and valAccoutN.Source_Lang = FND_GLOBAL.Current_Language
-			ON valAccout.Value = tbl_codeX.Segment2 and valAccout.ATTRIBUTE_CATEGORY = 'ACM_Account'
-            Inner Join  XLE_ENTITY_PROFILES xlep
-			ON tbl_codeX.SEGMENT1 = xlep.LEGAL_ENTITY_IDENTIFIER
-		ON tbl_codeX.CODE_COMBINATION_ID = tbl_map.VALUE_CODE_COMBINATION_ID 
-	On tbl_map.MAPPING_SET_CODE = tbl_mapb.MAPPING_SET_CODE
+Left Join XLA_MAPPING_SET_VALUES tbl_map
+	Inner Join XLA_MAPPING_SETS_B tbl_mapb 
+	ON  tbl_mapb.MAPPING_SET_CODE = tbl_map.MAPPING_SET_CODE  
+	And tbl_mapb.Application_Id =  '201' 
+	and tbl_mapb.Owner_Code = 'C' 
+	and tbl_mapb.ENABLED_FLAG = 'Y'
+    Inner Join GL_CODE_COMBINATIONS tbl_codeX
+		Inner Join FND_VS_VALUES_B valAccout
+			Inner Join FND_VS_VALUES_TL valAccoutN 
+			ON valAccout.Value_Id = valAccoutN.Value_Id 
+			and valAccoutN.LANGUAGE= FND_GLOBAL.Current_Language
+			and valAccoutN.Source_Lang = FND_GLOBAL.Current_Language
+		ON valAccout.Value = tbl_codeX.Segment2 and valAccout.ATTRIBUTE_CATEGORY = 'ACM_Account'
+        Inner Join  XLE_ENTITY_PROFILES xlep
+		ON tbl_codeX.SEGMENT1 = xlep.LEGAL_ENTITY_IDENTIFIER
+	ON tbl_codeX.CODE_COMBINATION_ID = tbl_map.VALUE_CODE_COMBINATION_ID 
 On to_char(tbl_cat.CATEGORY_ID) = tbl_map.INPUT_VALUE_CONSTANT2 and tbl_mapb.ENABLED_FLAG <> 'N' 
 Where tbl_cat.END_DATE_ACTIVE IS NULL  
 and xlep.NAME IN (:CompanyName)
