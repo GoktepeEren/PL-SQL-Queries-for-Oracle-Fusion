fd10d9ef5a5b1e8c9329a0036cb3c4c5dbe0c3fa
GoktepeEren <goktepeeren@yahoo.com>
2020-08-24 12:27:02 +0300

    TaxLookup new filter added

--- Tax_LookupCodes_WRefSets.sql
+++ Tax_LookupCodes_WRefSets.sql
@@ -12,6 +12,6 @@ From
 FND_LOOKUP_VALUES fnd_tax
     Inner Join FND_SETID_SETS_VL fnd_set  
     ON fnd_tax.Set_Id = fnd_set.Set_Id 
-Where fnd_tax.LOOKUP_TYPE =  'ZX_INPUT_CLASSIFICATIONS'
+Where fnd_tax.LOOKUP_TYPE IN ('ZX_INPUT_CLASSIFICATIONS', 'ZX_OUTPUT_CLASSIFICATIONS', 'ZX_WHT_TAX_CLASSIFICATION_CODE', 'ZX_WEB_EXP_TAX_CLASSIFICATIONS')
 and fnd_tax.Language In (:LanguageTax) and fnd_tax.Source_Lang IN (:SourLanguageTax)
 Order By fnd_tax.Creation_Date DESC
